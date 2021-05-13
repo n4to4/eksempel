9b4bf7c173bc7a81c633e1f387179c4a34b89a1d
n4to4 <n4to4k@gmail.com>
2021-05-14 07:55:31 +0900

    snapshot

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,10 @@ where
     s.as_ref().len()
 }
 
+pub fn strlen_dyn(s: &dyn AsRef<str>) -> usize {
+    s.as_ref().len()
+}
+
 pub fn bool_then<T>(b: bool, f: impl FnOnce() -> T) -> Option<T> {
     if b {
         Some(f())
@@ -27,10 +31,42 @@ impl Hei for &str {
     }
 }
 
-pub fn foo() {
-    "J".hei();
+impl Hei for String {
+    fn hei(&self) {
+        println!("hei {}", self);
+    }
 }
 
-pub fn bar(h: impl Hei) {
-    h.hei();
+pub fn foo() {
+    //bar(&["J", "Jon"]);
+    //bar(&[String::from("J"), String::from("Jon")]);
+    bar(&[Box::new("J"), Box::new(String::from("Jon"))]);
+}
+
+pub fn bar(s: &[Box<dyn Hei>]) {
+    for h in s {
+        h.hei();
+    }
+}
+
+pub fn sized_or_not() {
+    fn is_sized<T>() {}
+
+    //is_sized::<dyn Hei>();
+
+    //is_sized::<str>();
+    is_sized::<&str>();
+
+    //is_sized::<[u8]>();
+    is_sized::<&[u8]>();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn runit() {
+        foo();
+    }
 }
